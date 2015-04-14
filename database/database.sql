67100939e15e3fe419f535c295feff3ad40a9c9d
MichaelACohen <michael@cohenhome.net>
2015-04-14 14:57:55 -0700

    sql file works i think

--- database/database.sql
+++ database/database.sql
@@ -2,31 +2,36 @@ DROP DATABASE IF EXISTS db;
 
 CREATE DATABASE db;
 
+GRANT ALL PRIVILEGES ON db.* to root@localhost IDENTIFIED BY 'michael';
+
 USE db;
---testing git
---added role just in case
+
+#testing git
+#added role just in case
+
 CREATE TABLE Users (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	display_name text NOT NULL,
-	username text NOT NULL UNIQUE,
+	username VARCHAR(32) NOT NULL UNIQUE,
 	password VARCHAR(32) NOT NULL,
 	role ENUM('user', 'administrator') NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE Videos (
 	vid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	url text NOT NULL UNIQUE
+	url VARCHAR(100) NOT NULL UNIQUE
 ) ENGINE=InnoDB;
 
 CREATE TABLE Hashtags (
 	hid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	tag TEXT NOT NULL UNIQUE
-)
+	tag VARCHAR(50) NOT NULL UNIQUE
+) ENGINE=InnoDB;
 
---This contains a row for each video/hashtag combo
---So if a video with id 1 is uploaded with 
---hashtags with ids 1 and 2,
---we will insert two rows { (1, 1), (1, 2) } in table
+#This contains a row for each video/hashtag combo
+#So if a video with id 1 is uploaded with 
+#hashtags with ids 1 and 2,
+#we will insert two rows { (1, 1), (1, 2) } in table
+
 CREATE TABLE VideoHashtags (
 	videoID INT UNSIGNED NOT NULL,
 	hashtagID INT UNSIGNED NOT NULL,
@@ -35,7 +40,7 @@ CREATE TABLE VideoHashtags (
 		REFERENCES Videos(vid),
 	FOREIGN KEY (hashtagID)
 		REFERENCES Hashtags(hid)
-)
+) ENGINE=InnoDB;
 
 CREATE TABLE Likes (
 	userID INT UNSIGNED NOT NULL,
@@ -47,7 +52,7 @@ CREATE TABLE Likes (
 		REFERENCES Videos(vid)
 ) ENGINE=InnoDB;
 
---Check Robin's nest example to see if this should have primary key
+#Check Robin's nest example to see if this should have primary key
 CREATE TABLE Comments (
 	userID INT UNSIGNED NOT NULL,
 	videoID INT UNSIGNED NOT NULL,
@@ -67,5 +72,3 @@ CREATE TABLE Follows (
 	FOREIGN KEY (followeeID)
 		REFERENCES Users(id)
 ) ENGINE=InnoDB;
-
-
---
+++ database/db_info.php
@@ -0,0 +1,7 @@
+<?php
+  //database login info
+  $hn = 'localhost';
+  $db = 'db';
+  $un = 'root';
+  $pw = 'michael';
+?>
