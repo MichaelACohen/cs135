4dd5d4c056a9030415a1c843ced4866f5321d2bd
MichaelACohen <michael@cohenhome.net>
2015-04-21 17:32:53 -0700

    video positioning stuff

--- application/profile.php
+++ application/profile.php
@@ -20,50 +20,77 @@ if($_POST){
             if ($row[0]) {
             //video already exist link it to id in Videofeed
             
-            $conn->close();
             } else {
             //put video in Videos
             //put video in Videofeed
-
-            $conn->close();
-      
-      
             }
         }
     }
 
 }
-    
-$conn->close();
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <title>Profile</title>
-        <?php require_once 'navBar.php' ?>
-        Here is your profile, <?php echo $_SESSION['displayName'];?>. <br>
-        
-        <form method="post">
-            <table>
-                <tr>
-                <td><input type="text" name="vidurl"/></td>
-                <td>Add Video URL</td>
-                </tr>
-            </table>
-            <input type="submit"/>
-        </form>
-        
-       <iframe width="420" height="315"
-       src=https://www.youtube.com/embed/3aF-ANf4KY8>
-        </iframe>
-        
-       
-
-        
+    <title>Profile</title>
+    <style>
+        #vidList {
+            text-align:center;
+        }
+        .videoWrapper {
+            position: relative;
+            padding-top: 25px;
+        }
+    </style>
 </head>
 <body>
+    <?php require_once 'navBar.php' ?>
+    Here is your profile, <?php echo $_SESSION['displayName'];?>. <br>
+
+    <form method="post">
+        <table>
+            <tr>
+            <td><input type="text" name="vidurl"/></td>
+            <td>Add Video URL</td>
+            </tr>
+        </table>
+        <input type="submit"/>
+    </form>
+    <div id="vidList">
+        <div class="videoWrapper">
+            <iframe width="420" height="315"
+           src="https://www.youtube.com/embed/3aF-ANf4KY8" frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+        <div class="videoWrapper">
+            <iframe width="420" height="315"
+           src="https://www.youtube.com/embed/B38wu7wxGjg" frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+        <?php
+            $id = $_SESSION['id'];
+            $query = "SELECT * FROM VideoFeed WHERE userID='$id'";
+            $result = $conn->query($query);
+            if (!$result) {
+                die($conn->error);
+            } else {
+                $rows = $result->num_rows;
+                if ($rows) {
+                    for ($i = 0; $i < $rows; ++$i) {
+                        $result->data_seek($i);
+                        $url = $result->fetch_assoc()['url'];
+                        echo '<iframe width="420" height="315"
+                        src=$id frameborder="0" allowfullscreen></iframe>';
+                    }
+                }
+                $result->close();
+                $conn->close();
+            }
+        ?>
+    </div>
+</body>
 
 <?php
 function validate($data) {
