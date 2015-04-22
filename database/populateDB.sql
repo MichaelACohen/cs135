5d96657dbf2b6f02463ed42f8465a09c2bcb7d16
MichaelACohen <michael@cohenhome.net>
2015-04-22 11:24:09 -0700

    fixed

--- database/populateDB.sql
+++ database/populateDB.sql
@@ -28,14 +28,14 @@ INSERT INTO Videos (youtubeID) VALUES ("3aF-ANf4KY8");
 INSERT INTO Videos (youtubeID) VALUES ("B38wu7wxGjg");
 INSERT INTO Videos (youtubeID) VALUES ("fWNaR-rxAic");
 INSERT INTO Videos (youtubeID) VALUES ("0pajUiPYjrI");
-INSERT INTO Videos (youtubeID) VALUES ("qnHyhCYOgTI")
+INSERT INTO Videos (youtubeID) VALUES ("qnHyhCYOgTI");
 
 #videofeed
-INSERT INTO VideoFeed (userID, videoID) VALUES (0, 0);
-INSERT INTO VideoFeed (userID, videoID) VALUES (0, 4);
 INSERT INTO VideoFeed (userID, videoID) VALUES (1, 1);
+INSERT INTO VideoFeed (userID, videoID) VALUES (1, 5);
 INSERT INTO VideoFeed (userID, videoID) VALUES (2, 2);
 INSERT INTO VideoFeed (userID, videoID) VALUES (3, 3);
+INSERT INTO VideoFeed (userID, videoID) VALUES (4, 4);
 
 #hashtags
 INSERT INTO Hashtags (tag) VALUES ("pomona");
@@ -45,49 +45,49 @@ INSERT INTO Hashtags (tag) VALUES ("cats");
 INSERT INTO Hashtags (tag) VALUES ("kobe");
 
 #video hashtags
-INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (0, 0);
 INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (1, 1);
 INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (2, 2);
 INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (3, 3);
 INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (4, 4);
+INSERT INTO VideoHashtags (videoID, hashtagID) VALUES (5, 5);
 
 #likes
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (1, 0, 0);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (0, 0, 0);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (0, 1, 1);
 INSERT INTO Likes (liker, videoOwner, videoID) VALUES (2, 1, 1);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (3, 1, 1);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (0, 2, 2);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (1, 1, 1);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (1, 2, 2);
 INSERT INTO Likes (liker, videoOwner, videoID) VALUES (3, 2, 2);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (4, 2, 2);
 INSERT INTO Likes (liker, videoOwner, videoID) VALUES (1, 3, 3);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (3, 3, 3);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (4, 3, 3);
 INSERT INTO Likes (liker, videoOwner, videoID) VALUES (2, 4, 4);
-INSERT INTO Likes (liker, videoOwner, videoID) VALUES (0, 4, 4);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (4, 4, 4);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (3, 1, 5);
+INSERT INTO Likes (liker, videoOwner, videoID) VALUES (1, 1, 5);
 
 #comments
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (2, 0, 0, "this school sucks");
+VALUES (3, 1, 1, "this school sucks");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (0, 0, 0, "lol");
+VALUES (1, 1, 1, "lol");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (3, 1, 1, "nice");
+VALUES (4, 2, 2, "nice");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (1, 1, 1, "thanks");
+VALUES (2, 2, 2, "thanks");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (0, 2, 2, "cute");
+VALUES (1, 3, 3, "cute");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (3, 3, 3, "cats");
+VALUES (4, 4, 4, "cats");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (2, 0, 4, "yes michael");
+VALUES (3, 1, 5, "yes michael");
 INSERT INTO Comments (commenter, videoOwner, videoID, message)
-VALUES (2, 0, 4, "Kobe.");
+VALUES (3, 1, 5, "Kobe.");
 
 #follows
-INSERT INTO Follows (followerID, followeeID) VALUES (0, 1);
-INSERT INTO Follows (followerID, followeeID) VALUES (0, 2);
-INSERT INTO Follows (followerID, followeeID) VALUES (0, 3);
-INSERT INTO Follows (followerID, followeeID) VALUES (1, 0);
+INSERT INTO Follows (followerID, followeeID) VALUES (1, 2);
 INSERT INTO Follows (followerID, followeeID) VALUES (1, 3);
-INSERT INTO Follows (followerID, followeeID) VALUES (2, 0);
-INSERT INTO Follows (followerID, followeeID) VALUES (3, 0);
+INSERT INTO Follows (followerID, followeeID) VALUES (1, 4);
+INSERT INTO Follows (followerID, followeeID) VALUES (2, 1);
+INSERT INTO Follows (followerID, followeeID) VALUES (2, 4);
 INSERT INTO Follows (followerID, followeeID) VALUES (3, 1);
+INSERT INTO Follows (followerID, followeeID) VALUES (4, 1);
+INSERT INTO Follows (followerID, followeeID) VALUES (4, 2);
